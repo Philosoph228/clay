d4f4e95abaf28520800b25e7231dd8016f21c090
Dulappy <92800639+Dulappy@users.noreply.github.com>
2025-03-26 01:35:01 +0200

    Add support for semi-transparent rectangles and improve borders
    The way semi-transparent rounded rectangles are rendered requires looping through every pixel affected and changing the alpha value. I wonder if something can be done through GDI.
    
    This change also makes use of the AlphaBlend API, which means that linking msimg.dll is required and that Windows NT 4.0 and Windows 95 are no longer supported.

--- renderers/win32_gdi/clay_renderer_gdi.c
+++ renderers/win32_gdi/clay_renderer_gdi.c
@@ -5,6 +5,92 @@ HDC renderer_hdcMem = {0};
 HBITMAP renderer_hbmMem = {0};
 HANDLE renderer_hOld = {0};
 
+BOOL DrawTransparentRect(HDC hdc, RECT rc, HBRUSH brush, uint8_t opacity)
+{
+    HDC tempHdc = CreateCompatibleDC(hdc);
+    BLENDFUNCTION blend = { AC_SRC_OVER, 0, opacity, 0 };
+    RECT drawrc = { 0, 0, rc.right - rc.left, rc.bottom - rc.top };
+
+    HBITMAP bmp;
+    BITMAPINFO bmi;
+
+    ZeroMemory(&bmi, sizeof(BITMAPINFO));
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = rc.right - rc.left;
+    bmi.bmiHeader.biHeight = rc.bottom - rc.top;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32; // 32bpp bitmap
+    bmi.bmiHeader.biCompression = BI_RGB;
+    bmi.bmiHeader.biSizeImage = (rc.right - rc.left) * (rc.bottom - rc.top) * 4;
+
+    bmp = CreateDIBSection(tempHdc, &bmi, DIB_RGB_COLORS, NULL, NULL, 0x0);
+    SelectObject(tempHdc, bmp);
+
+    FillRect(tempHdc, &drawrc, brush);
+    BOOL rv = AlphaBlend(hdc, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, tempHdc, 0, 0, drawrc.right - drawrc.left, drawrc.bottom - drawrc.top, blend);
+
+    DeleteObject(bmp);
+    DeleteDC(tempHdc);
+
+    return rv;
+}
+
+BOOL DrawTransparentRgn(HDC hdc, RECT rc, HRGN rgn, Clay_Color color, uint8_t opacity)
+{
+    HDC tempHdc = CreateCompatibleDC(hdc);
+    BLENDFUNCTION blend = { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };
+    RECT drawrc = { 0, 0, rc.right - rc.left, rc.bottom - rc.top };
+
+    HBITMAP bmp;
+    BITMAPINFO bmi;
+
+    ZeroMemory(&bmi, sizeof(BITMAPINFO));
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = rc.right - rc.left;
+    bmi.bmiHeader.biHeight = rc.bottom - rc.top;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32; // 32bpp bitmap
+    bmi.bmiHeader.biCompression = BI_RGB;
+    bmi.bmiHeader.biSizeImage = (rc.right - rc.left) * (rc.bottom - rc.top) * 4;
+
+    void* pBits;
+    bmp = CreateDIBSection(tempHdc, &bmi, DIB_RGB_COLORS, &pBits, NULL, 0x0);
+
+    COLORREF premultClr = RGB(
+        color.r * color.a / 255, 
+        color.g * color.a / 255,
+        color.b * color.a / 255
+    );
+    HBRUSH brush = CreateSolidBrush(premultClr);
+
+    // Set the bitmap to opaque white
+    memset(pBits, 0xFF, bmi.bmiHeader.biSizeImage);
+
+    SelectObject(tempHdc, bmp);
+    SetBkMode(tempHdc, TRANSPARENT);
+
+    OffsetRgn(rgn, -rc.left, -rc.top);
+    FillRgn(tempHdc, rgn, brush);
+
+    DWORD* pixel = (DWORD*)pBits;
+    for (int i = 0; i < bmi.bmiHeader.biWidth * bmi.bmiHeader.biHeight; i++) {
+        if ((pixel[i] & 0xFF000000) == 0) { // If alpha is 0 (GDI sets the alpha to 0 by default in the area it operates on)
+            pixel[i] |= (opacity << 24); // Set the alpha channel
+        }
+        else {
+            pixel[i] = 0; // Make the pixel transparent black.
+        }
+    }
+
+    BOOL rv = AlphaBlend(hdc, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, tempHdc, 0, 0, drawrc.right - drawrc.left, drawrc.bottom - drawrc.top, blend);
+
+    DeleteObject(bmp);
+    DeleteObject(brush);
+    DeleteDC(tempHdc);
+
+    return rv;
+}
+
 void Clay_Win32_Render(HWND hwnd, Clay_RenderCommandArray renderCommands, LPCSTR* fonts)
 {
     bool is_clipping = false;
@@ -76,12 +162,22 @@ void Clay_Win32_Render(HWND hwnd, Clay_RenderCommandArray renderCommands, LPCSTR
                     r.left, r.top, r.right + 1, r.bottom + 1,
                     rrd.cornerRadius.topLeft * 2, rrd.cornerRadius.topLeft * 2);
 
-                FillRgn(renderer_hdcMem, roundedRectRgn, recColor);
+                if (rrd.backgroundColor.a == 255.f) {
+                    FillRgn(renderer_hdcMem, roundedRectRgn, recColor);
+                }
+                else {
+                    DrawTransparentRgn(renderer_hdcMem, r, roundedRectRgn, rrd.backgroundColor, rrd.backgroundColor.a);
+                }
                 DeleteObject(roundedRectRgn);
             }
             else
             {
-                FillRect(renderer_hdcMem, &r, recColor);
+                if (rrd.backgroundColor.a == 255.f) {
+                    FillRect(renderer_hdcMem, &r, recColor);
+                }
+                else {
+                    DrawTransparentRect(renderer_hdcMem, r, recColor, rrd.backgroundColor.a);
+                }
             }
 
             DeleteObject(recColor);
@@ -124,10 +220,10 @@ void Clay_Win32_Render(HWND hwnd, Clay_RenderCommandArray renderCommands, LPCSTR
             Clay_BorderRenderData brd = renderCommand->renderData.border;
             RECT r = rc;
 
-            r.left = boundingBox.x;
-            r.top = boundingBox.y;
-            r.right = boundingBox.x + boundingBox.width;
-            r.bottom = boundingBox.y + boundingBox.height;
+            r.left = boundingBox.x + brd.width.left / 2;
+            r.top = boundingBox.y + brd.width.top / 2;
+            r.right = boundingBox.x + boundingBox.width - brd.width.right / 2;
+            r.bottom = boundingBox.y + boundingBox.height - brd.width.bottom / 2;
 
             LOGBRUSH penAttributes = {
                 BS_SOLID,
@@ -144,58 +240,96 @@ void Clay_Win32_Render(HWND hwnd, Clay_RenderCommandArray renderCommands, LPCSTR
 
             if (brd.cornerRadius.topLeft == 0)
             {
-                MoveToEx(renderer_hdcMem, r.left - 0.5f * brd.width.left, r.top, NULL);
-                LineTo(renderer_hdcMem, r.right + 0.5f * brd.width.right, r.top);
+                if (brd.width.top) {
+                    MoveToEx(renderer_hdcMem, r.left - 0.5f * brd.width.left, r.top, NULL);
+                    LineTo(renderer_hdcMem, r.right + 0.5f * brd.width.right, r.top);
+                }
 
-                SelectObject(renderer_hdcMem, leftPen);
-                MoveToEx(renderer_hdcMem, r.left, r.top - 0.5f * brd.width.top, NULL);
-                LineTo(renderer_hdcMem, r.left, r.bottom + 0.5f * brd.width.bottom);
+                if (brd.width.left) {
+                    SelectObject(renderer_hdcMem, leftPen);
+                    MoveToEx(renderer_hdcMem, r.left, r.top - 0.5f * brd.width.top, NULL);
+                    LineTo(renderer_hdcMem, r.left, r.bottom + 0.5f * brd.width.bottom);
+                }
 
-                SelectObject(renderer_hdcMem, bottomPen);
-                MoveToEx(renderer_hdcMem, r.left - 0.5f * brd.width.left, r.bottom, NULL);
-                LineTo(renderer_hdcMem, r.right + 0.5f * brd.width.right, r.bottom);
+                if (brd.width.bottom) {
+                    SelectObject(renderer_hdcMem, bottomPen);
+                    MoveToEx(renderer_hdcMem, r.left - 0.5f * brd.width.left, r.bottom, NULL);
+                    LineTo(renderer_hdcMem, r.right + 0.5f * brd.width.right, r.bottom);
+                }
 
-                SelectObject(renderer_hdcMem, rightPen);
-                MoveToEx(renderer_hdcMem, r.right, r.top + 0.5f * brd.width.top, NULL);
-                LineTo(renderer_hdcMem, r.right, r.bottom + 0.5f * brd.width.bottom);
+                if (brd.width.right) {
+                    SelectObject(renderer_hdcMem, rightPen);
+                    MoveToEx(renderer_hdcMem, r.right, r.top + 0.5f * brd.width.top, NULL);
+                    LineTo(renderer_hdcMem, r.right, r.bottom + 0.5f * brd.width.bottom);
+                }
             }
             else
             {
                 // TODO: Clean up this mess so it's readable
 
-                RECT arcRC_TL = { r.left, r.top, r.left + 2 * brd.cornerRadius.topLeft, r.top + 2 * brd.cornerRadius.topLeft };
-                RECT arcRC_TR = { r.right - 2 * brd.cornerRadius.topRight, r.top, r.right, r.top + 2 * brd.cornerRadius.topRight };
-                RECT arcRC_BL = { r.left, r.bottom - 2 * brd.cornerRadius.bottomLeft, r.left + 2 * brd.cornerRadius.topLeft, r.bottom };
-                RECT arcRC_BR = { r.right - 2 * brd.cornerRadius.bottomLeft, r.bottom - 2 * brd.cornerRadius.bottomLeft, r.right, r.bottom };
+                RECT arcRC_TL = { 
+                    r.left, 
+                    r.top, 
+                    r.left + 2 * brd.cornerRadius.topLeft - brd.width.top / 2,
+                    r.top + 2 * brd.cornerRadius.topLeft - brd.width.left / 2
+                };
+                RECT arcRC_TR = { 
+                    r.right - 2 * brd.cornerRadius.topRight + brd.width.top / 2, 
+                    r.top, 
+                    r.right, 
+                    r.top + 2 * brd.cornerRadius.topRight - brd.width.right / 2
+                };
+                RECT arcRC_BL = { 
+                    r.left, 
+                    r.bottom - 2 * brd.cornerRadius.bottomLeft + brd.width.left / 2, 
+                    r.left + 2 * brd.cornerRadius.topLeft - brd.width.bottom / 2, 
+                    r.bottom 
+                };
+                RECT arcRC_BR = { 
+                    r.right - 2 * brd.cornerRadius.bottomLeft + brd.width.bottom / 2, 
+                    r.bottom - 2 * brd.cornerRadius.bottomLeft + brd.width.right / 2, 
+                    r.right, 
+                    r.bottom 
+                };
 
                 SetArcDirection(renderer_hdcMem, AD_CLOCKWISE);
-                Arc(renderer_hdcMem, arcRC_TL.left, arcRC_TL.top, arcRC_TL.right, arcRC_TL.bottom, 
-                    arcRC_TL.left, (arcRC_TL.top + arcRC_TL.bottom) / 2, (arcRC_TL.left + arcRC_TL.right) / 2, arcRC_TL.top);
 
-                MoveToEx(renderer_hdcMem, (arcRC_TL.left + arcRC_TL.right) / 2, r.top, NULL);
-                LineTo(renderer_hdcMem, (arcRC_TR.left + arcRC_TR.right) / 2, r.top);
+                if (brd.width.top) {
+                    Arc(renderer_hdcMem, arcRC_TL.left, arcRC_TL.top, arcRC_TL.right, arcRC_TL.bottom,
+                        arcRC_TL.left, (arcRC_TL.top + arcRC_TL.bottom) / 2, (arcRC_TL.left + arcRC_TL.right) / 2, arcRC_TL.top);
 
-                SelectObject(renderer_hdcMem, rightPen);
-                Arc(renderer_hdcMem, arcRC_TR.left, arcRC_TR.top, arcRC_TR.right, arcRC_TR.bottom,
-                    (arcRC_TR.left + arcRC_TR.right) / 2, arcRC_TR.top, arcRC_TR.right, (arcRC_TR.top + arcRC_TR.bottom) / 2);
+                    MoveToEx(renderer_hdcMem, (arcRC_TL.left + arcRC_TL.right) / 2, r.top, NULL);
+                    LineTo(renderer_hdcMem, (arcRC_TR.left + arcRC_TR.right) / 2, r.top);
+                }
 
-                MoveToEx(renderer_hdcMem, r.right, (arcRC_TR.top + arcRC_TR.bottom) / 2, NULL);
-                LineTo(renderer_hdcMem, r.right, (arcRC_BR.top + arcRC_BR.bottom) / 2);
+                if (brd.width.right) {
+                    SelectObject(renderer_hdcMem, rightPen);
+                    Arc(renderer_hdcMem, arcRC_TR.left, arcRC_TR.top, arcRC_TR.right, arcRC_TR.bottom,
+                        (arcRC_TR.left + arcRC_TR.right) / 2, arcRC_TR.top, arcRC_TR.right, (arcRC_TR.top + arcRC_TR.bottom) / 2);
 
-                SelectObject(renderer_hdcMem, leftPen);
+                    MoveToEx(renderer_hdcMem, r.right, (arcRC_TR.top + arcRC_TR.bottom) / 2, NULL);
+                    LineTo(renderer_hdcMem, r.right, (arcRC_BR.top + arcRC_BR.bottom) / 2);
+                }
+                
                 SetArcDirection(renderer_hdcMem, AD_COUNTERCLOCKWISE);
-                Arc(renderer_hdcMem, arcRC_BL.left, arcRC_BL.top, arcRC_BL.right, arcRC_BL.bottom,
-                    arcRC_BL.left, (arcRC_BL.top + arcRC_BL.bottom) / 2, (arcRC_BL.left + arcRC_BL.right) / 2, arcRC_BL.bottom);
 
-                MoveToEx(renderer_hdcMem, r.left, (arcRC_TL.top + arcRC_TL.bottom) / 2, NULL);
-                LineTo(renderer_hdcMem, r.left, (arcRC_BL.top + arcRC_BL.bottom) / 2);
+                if (brd.width.left) {
+                    SelectObject(renderer_hdcMem, leftPen);
+                    Arc(renderer_hdcMem, arcRC_BL.left, arcRC_BL.top, arcRC_BL.right, arcRC_BL.bottom,
+                        arcRC_BL.left, (arcRC_BL.top + arcRC_BL.bottom) / 2, (arcRC_BL.left + arcRC_BL.right) / 2, arcRC_BL.bottom);
 
-                SelectObject(renderer_hdcMem, bottomPen);
-                Arc(renderer_hdcMem, arcRC_BR.left, arcRC_BR.top, arcRC_BR.right, arcRC_BR.bottom,
-                    (arcRC_BR.left + arcRC_BR.right) / 2, arcRC_BR.bottom, arcRC_BR.right, (arcRC_BR.top + arcRC_BR.bottom) / 2);
+                    MoveToEx(renderer_hdcMem, r.left, (arcRC_TL.top + arcRC_TL.bottom) / 2, NULL);
+                    LineTo(renderer_hdcMem, r.left, (arcRC_BL.top + arcRC_BL.bottom) / 2);
+                }
 
-                MoveToEx(renderer_hdcMem, (arcRC_BL.left + arcRC_BL.right) / 2, r.bottom, NULL);
-                LineTo(renderer_hdcMem, (arcRC_BR.left + arcRC_BR.right) / 2, r.bottom);
+                if (brd.width.bottom) {
+                    SelectObject(renderer_hdcMem, bottomPen);
+                    Arc(renderer_hdcMem, arcRC_BR.left, arcRC_BR.top, arcRC_BR.right, arcRC_BR.bottom,
+                        (arcRC_BR.left + arcRC_BR.right) / 2, arcRC_BR.bottom, arcRC_BR.right, (arcRC_BR.top + arcRC_BR.bottom) / 2);
+
+                    MoveToEx(renderer_hdcMem, (arcRC_BL.left + arcRC_BL.right) / 2, r.bottom, NULL);
+                    LineTo(renderer_hdcMem, (arcRC_BR.left + arcRC_BR.right) / 2, r.bottom);
+                }
                 
             }
 
